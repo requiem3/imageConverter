ee65df50a5339109388a2ca89ffc7ac84a1c1afd
requiem3 <kyle.heitman@uconn.edu>
2014-09-18 15:06:00 -0400

    Converts images to other file types

---
+++ ImageConverter/Form1.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class imgfrm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(imgfrm))
+        Me.btnbrowse = New System.Windows.Forms.Button()
+        Me.btnconvert = New System.Windows.Forms.Button()
+        Me.savedialog = New System.Windows.Forms.SaveFileDialog()
+        Me.OFdialog = New System.Windows.Forms.OpenFileDialog()
+        Me.cbformats = New System.Windows.Forms.ComboBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnbrowse
+        '
+        Me.btnbrowse.Location = New System.Drawing.Point(9, 229)
+        Me.btnbrowse.Name = "btnbrowse"
+        Me.btnbrowse.Size = New System.Drawing.Size(85, 23)
+        Me.btnbrowse.TabIndex = 2
+        Me.btnbrowse.Text = "Browse"
+        Me.btnbrowse.UseVisualStyleBackColor = True
+        '
+        'btnconvert
+        '
+        Me.btnconvert.Location = New System.Drawing.Point(103, 229)
+        Me.btnconvert.Name = "btnconvert"
+        Me.btnconvert.Size = New System.Drawing.Size(75, 23)
+        Me.btnconvert.TabIndex = 3
+        Me.btnconvert.Text = "Convert"
+        Me.btnconvert.UseVisualStyleBackColor = True
+        '
+        'OFdialog
+        '
+        Me.OFdialog.FileName = "OpenFileDialog1"
+        '
+        'cbformats
+        '
+        Me.cbformats.FormattingEnabled = True
+        Me.cbformats.Items.AddRange(New Object() {"BMP", "EMF", "GIF", "ICO", "JPEG", "PNG "})
+        Me.cbformats.Location = New System.Drawing.Point(184, 231)
+        Me.cbformats.Name = "cbformats"
+        Me.cbformats.Size = New System.Drawing.Size(121, 21)
+        Me.cbformats.TabIndex = 4
+        Me.cbformats.Text = "Image Type"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(6, 9)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(286, 174)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(3, 34)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(302, 189)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(307, 24)
+        Me.MenuStrip1.TabIndex = 6
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'imgfrm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(307, 255)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.cbformats)
+        Me.Controls.Add(Me.btnconvert)
+        Me.Controls.Add(Me.btnbrowse)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "imgfrm"
+        Me.Text = "Image Converter"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnbrowse As System.Windows.Forms.Button
+    Friend WithEvents btnconvert As System.Windows.Forms.Button
+    Friend WithEvents savedialog As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents OFdialog As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents cbformats As System.Windows.Forms.ComboBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ ImageConverter/Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class imgfrm
+    Dim val As Boolean
+
+    Private Sub btnbrowse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnbrowse.Click
+
+        Try
+            If OFDialog.ShowDialog <> Windows.Forms.DialogResult.Cancel Then
+                PictureBox1.Image = Image.FromFile(OFDialog.FileName)
+                PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+                val = True
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnconvert_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnconvert.Click
+        Try
+            If val = True Then
+                If cbformats.SelectedItem = "PNG" Then
+                    saveDialog.Filter = "PNG|*.png"
+                    If savedialog.ShowDialog <> Windows.Forms.DialogResult.Cancel Then PictureBox1.Image.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Png)
+
+                ElseIf cbformats.SelectedItem = "JPEG" Then
+                    saveDialog.Filter = "JPEG|*.jpg"
+                    If saveDialog.ShowDialog <> Windows.Forms.DialogResult.Cancel Then PictureBox1.Image.Save(saveDialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg)
+                ElseIf cbformats.SelectedItem = "BMP" Then
+                    saveDialog.Filter = "BMP|*.bmp"
+                    If saveDialog.ShowDialog <> Windows.Forms.DialogResult.Cancel Then PictureBox1.Image.Save(saveDialog.FileName, System.Drawing.Imaging.ImageFormat.Bmp)
+                ElseIf cbformats.SelectedItem = "GIF" Then
+                    saveDialog.Filter = "GIF|*.gif"
+                    If savedialog.ShowDialog <> Windows.Forms.DialogResult.Cancel Then PictureBox1.Image.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Gif)
+                ElseIf cbformats.SelectedItem = "TIFF" Then
+                    savedialog.Filter = "TIFF|*.tif"
+                    If savedialog.ShowDialog <> Windows.Forms.DialogResult.Cancel Then PictureBox1.Image.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Tiff)
+                ElseIf cbformats.SelectedItem = "EMF" Then
+                    savedialog.Filter = "EMF|*.emf"
+                    If savedialog.ShowDialog <> Windows.Forms.DialogResult.Cancel Then PictureBox1.Image.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Emf)
+                ElseIf cbformats.SelectedItem = "ICO" Then
+                    savedialog.Filter = "ICO|*.ico"
+                    If savedialog.ShowDialog <> Windows.Forms.DialogResult.Cancel Then PictureBox1.Image.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Icon)
+
+
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub HelpToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HelpToolStripMenuItem.Click
+        Form2.Show()
+    End Sub
+End Class
